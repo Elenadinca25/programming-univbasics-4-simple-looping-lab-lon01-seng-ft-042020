898038422bb90b0b8f218f4c90ceb29fbd5fa91b
Elena Dinca <elenadinca25@gmail.com>
2020-04-02 14:37:04 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,4 +1,17 @@
 # Write your methods here
+
+def method_name(parameter)
+  count = 0
+  while count < (hello)
+    puts "hello"
+    count += 1
+  end
+end
+
+
+
+
+
 def loop_message_five_times(message)
   count = 0
   while count < 5 do
