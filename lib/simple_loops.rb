243b453fce4481026d8131e43afd8ef21b992f8d
Elena Dinca <elenadinca25@gmail.com>
2020-04-02 14:46:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -34,3 +34,12 @@ def output_array(array)
     count += 1
   end
 end
+
+
+def return_string_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count].to_s
+    count += 1
+  end
+end
